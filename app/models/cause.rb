fbe78bb48bc17abeb7e4f1b0ee58b87b887d0f39
Carolyn Gannon <cganon@gmail.com>
2015-01-18 18:09:44 -0500

    remove old models and migrations

--- app/models/cause.rb
+++
@@ -1,3 +0,0 @@
-class Cause < Keyword
-
-end
--- db/migrate/20150113043302_add_keyword_relationships.rb
+++
@@ -1,6 +0,0 @@
-class AddKeywordRelationships < ActiveRecord::Migration
-  def change
-    add_column :keywords, :parent_id, :integer
-    add_column :keywords, :child_id, :integer
-  end
-end
